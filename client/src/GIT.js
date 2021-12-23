3a74bfb5bb0fef615f37b356471400d2d5cbee14
Oleksandra Stepankovska <ostepank@softserveinc.com>
2021-12-23 08:21:00 +0200

    connect apollo

---
+++ client/src/GIT.js
@@ -0,0 +1,5 @@
+export const GITHUB_DATA = {
+    'token': 'YOUR_TOKEN',
+    'username': 'YOUR_USERNAME',
+    'GITHUB_BASE_URL': 'https://api.github.com/graphql'
+}
--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,25 +1,43 @@
 import React from "react";
 
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
 import Home from "../../pages/Home";
 import Header from '../Header';
 import About from "../../pages/About";
+import { GITHUB_DATA } from '../../GIT';
 
 import {
     BrowserRouter as Router,
     Routes,
     Route,
-  } from "react-router-dom";
+} from "react-router-dom";
 
+
+const link = new HttpLink({
+    uri: GITHUB_DATA.GITHUB_BASE_URL,
+    headers: {
+        authorisation: `Bearer ${GITHUB_DATA.token}`,
+
+    }
+})
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 const App: React.FC = () => {
     return(
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Router>
+        <ApolloProvider client={client}>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </Router>
+        </ApolloProvider>
     )
 };
 
