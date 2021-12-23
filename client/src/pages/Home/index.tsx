5c98440b386f16e8409ff72dfa7b7d7f8a436986
Oleksandra Stepankovska <ostepank@softserveinc.com>
2021-12-23 12:01:48 +0200

    trial query

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { HomeWrapper } from "./styles";
+import { gql, useQuery } from "@apollo/client";
 
 const Home: React.FC = () => {
+    const USER_DATA = gql`
+        query { 
+            viewer{
+                login
+            }
+        }
+    `;
+    
+    const { data } = useQuery(USER_DATA);
+
     return(
-        <HomeWrapper>Home</HomeWrapper>
+        <HomeWrapper>{data}</HomeWrapper>
     )
 };
 
